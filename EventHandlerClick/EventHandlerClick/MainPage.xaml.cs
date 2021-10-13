507fbfb82493c5fa2eeb7f072d6a53fdaff7c1d5
Naoki <20cm0135@jec.ac.jp>
2021-10-13 11:50:08 +0900

    C#が私のリポジトリーに...

---
+++ EventHandlerClick/EventHandlerClick/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EventHandlerClick
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void buttonText_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (sender.Equals(buttonText))
+            {
+                label.Text = "Button Clicked";
+            }
+            else if (sender.Equals(buttonNext))
+            {
+                Navigation.PushAsync(new NextPage());
+            }
+        }
+    }
+}
---
+++ HelloXamarin/HelloXamarin/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloXamarin
+{
+    // このクラスがエントリポイント
+    // Single Activityなので、このクラスはActivityと考えて良い (泥likeな考え方)
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        // アプリケーションの起動時に呼び出される
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+        }
+
+        // アプリケーションがバックグラウンドに移行した時に呼び出される
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+        }
+
+        // 
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+        // アプリケーションの終了
+    }
+}
