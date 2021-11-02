68a339c12570d007d3d7eb601599f3f0a4819a88
naoki-mrnk <20cm0135@jec.ac.jp>
2021-11-02 09:38:45 +0900

    reserve classroomの追加

---
+++ ReserveClassroom/ReserveClassroom/DisplayDataPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ReserveClassroom
+{
+    public partial class DisplayDataPage : ContentPage
+    {
+        public DisplayDataPage(String date, String time, String room)
+        {
+            InitializeComponent();
+
+            dateLabel.Text = date;
+            timeLabel.Text = time;
+            pickerLabel.Text = room;
+        }
+    }
+}
---
+++ ReserveClassroom/ReserveClassroom/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ReserveClassroom
+{
+    public partial class MainPage : ContentPage
+    {
+        public String date;
+        public String time;
+        public String room;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void reserveButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            date = dateText.Date.ToString("yyyy/MM/dd");
+            time = timeText.Time.ToString(@"hh\:mm");
+            room = roomName.SelectedItem.ToString();
+            Navigation.PushAsync(new DisplayDataPage(date, time, room));
+        }
+
+        void dateText_DateSelected(System.Object sender, Xamarin.Forms.DateChangedEventArgs e)
+        {
+        }
+    }
+}
